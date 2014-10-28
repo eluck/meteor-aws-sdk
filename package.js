ef86537a087ad40b234f349c0285dbc0b3665181
eluck <mail@eluck.me>
2014-10-28 14:14:01 +0300

    Remove 'blocking' package, update tests

--- package.js
+++ package.js
@@ -9,16 +9,13 @@ Npm.depends({
 });
 
 Package.on_use(function (api) {
-  api.use('blocking');
-
   api.export('AWS');
-
   api.add_files([
     'server.js'
   ], 'server');
 });
 
 Package.on_test(function (api) {
-  api.use(['aws-sdk', 'tinytest', 'test-helpers'], 'server');
+  api.use(['eluck:aws-sdk', 'tinytest', 'test-helpers'], 'server');
   api.add_files('tests.js', 'server');
 });
--- tests.js
+++ tests.js
@@ -7,10 +7,8 @@ Tinytest.add('aws-sdk', function (test) {
   catch (e) {
   }
   test.isTrue(isDefined, "AWS is not defined");
-  test.isTrue(Package['aws-sdk'].AWS, "Package.aws-sdk.AWS is not defined");
 
   var s3 = new AWS.S3();
-  test.isTrue(s3);
-  test.isTrue(s3.getObjectSync);
-  test.isTrue(s3.getObjectSync._blocking);
+  test.isTrue(s3, "Cannot get instance of AWS.S3");
+  test.isTrue(s3.putObject, "Cannot get reference of S3.putObject");
 });
