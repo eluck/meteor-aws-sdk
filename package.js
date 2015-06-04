663e78a83df1888032e39c5a63e1127b4b746c2b
eluck <mail@eluck.me>
2015-06-05 02:29:10 +0300

    Update aws-sdk to 2.1.33

--- package.js
+++ package.js
@@ -1,11 +1,11 @@
 Package.describe({
   summary: 'SDK for AWS services including Amazon S3, Amazon EC2, DynamoDB, and Amazon SWF',
-  version: '2.0.23_1',
+  version: '2.1.33',
   git: 'https://github.com/eluck/meteor-aws-sdk.git'
 });
 
 Npm.depends({
-  'aws-sdk': '2.0.23'
+  'aws-sdk': '2.1.33'
 });
 
 Package.on_use(function (api) {
